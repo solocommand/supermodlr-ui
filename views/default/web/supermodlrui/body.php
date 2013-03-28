5c16374638767a28a1c8c3f75b15792810f5135f
Josh Worden <josh.worden@cygnus.com>
2013-03-28 18:46:54 -0500

    Updated paths to js files

--- views/default/web/supermodlrui/body.php
+++ views/default/web/supermodlrui/body.php
@@ -1,7 +1,31 @@
-<?php $controller->view('header'); ?>
+<?php 
+	
+	$controller->view('header');
 
-<?php $controller->metatags('<meta name="viewport" content="width=device-width, initial-scale=1.0">'); ?>
-<?php $controller->css('/modules/supermodlr-ui/lib/bootstrap/css/bootstrap.min.css'); ?>
+	$controller->metatags('<meta name="viewport" content="width=device-width, initial-scale=1.0">');
+	$controller->css('/modules/supermodlr-ui/lib/bootstrap/css/bootstrap.min.css');
+	
+	// Hack...prevent js from being included twice by moving out of controller.php view to body template.
+	// @todo: Do this better. This technically works since body is only called for the initial load.
+
+		$controller->js('supermodlr/lib/angularjs/angular.js','headertags'); 
+		$controller->js('supermodlr/lib/angularjs/angular-resource.js','headertags');  
+		$controller->js('supermodlr/lib/angularui/build/angular-ui.js','headertags');  
+
+		$controller->js('supermodlr/lib/crypto-js/md5.js','headertags');   
+
+		$controller->js('supermodlr/lib/jqueryui/js/jquery-1.8.2.js');
+		$controller->js('supermodlr/lib/jqueryui/js/jquery-ui-1.9.2.custom.min.js');
+
+
+		$controller->css('/modules/supermodlr/lib/jqueryui/css/base/minified/jquery-ui.min.css');   
+
+		$controller->css('/modules/supermodlr/lib/jqueryui/css/ui-lightness/jquery.ui.theme.css');
+
+		$js = file_get_contents(MODPATH.'/supermodlr/views/supermodlr/default/web/js/controller.js');
+		$controller->js($js, 'headerinline');
+
+?>
 
 <style type="text/css">
 	.error {
@@ -20,7 +44,13 @@
 			</div>
 
 			<div class="span10">
-				<?php echo $controller->content(); ?>
+
+				<div ng-app="supermodlr">
+
+					<?= $controller->content(); ?>
+
+				</div>
+
 			</div>
 
 		</div>
